d4d150a8a92c04a5939af9e8131fc5a17870b6cf
kamalesh90-alt <itsmaddy498@gmail.com>
2024-08-13 16:39:12 +0530

    Add files via upload

---
+++ dictionarydemo/dictionarydemo/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Dictionary<int, string> empdetails;
+
+        empdetails = new Dictionary<int, string>();
+
+        empdetails.Add(100, "kamal");
+
+        empdetails.Add(102, "maddy");
+
+        empdetails.Add(103, "Manasa");
+
+        string ename = empdetails[100];
+
+        Console.WriteLine(ename);
+
+        SortedList<string, int> Marks;
+
+        Marks = new SortedList<string, int>();
+
+        Marks.Add("Maths", 80);
+
+        Marks.Add("Physics", 90);
+
+        Marks.Add("English", 65);
+
+        Marks.Add("Chemistry", 75);
+
+        foreach (string key in Marks.Keys)
+        {
+
+            Console.WriteLine(key + " " + Marks[key]);
+
+        }
+        Console.WriteLine(Marks["Maths"] +" " +"Maths");
+
+        Marks.Append;
+        
+    }
+}
